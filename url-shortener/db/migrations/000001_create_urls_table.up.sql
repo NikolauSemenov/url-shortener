cae62d5f78bd653431e0105fdfc304df31b5568c
Xybyyn <58708717+NikolauSemenov@users.noreply.github.com>
2025-11-19 18:13:09 +0500

    delete

--- url-shortener/db/migrations/000001_create_urls_table.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS urls(
-    original_url VARCHAR(500) NOT NULL,
-    alias VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (original_url, alias)
-);
